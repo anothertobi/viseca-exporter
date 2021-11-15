4aa1f81510d5bd39ded6e167dc82e438e59982d2
Tobias Nehrlich <tobi@anothertobi.io>
2021-11-16 00:22:17 +0100

    Complete data struct and extend CSV output

--- viseca-exporter.go
+++ viseca-exporter.go
@@ -13,9 +13,40 @@ type Transactions struct {
 }
 
 type Transaction struct {
-	Date         string  `json:"date"`
-	Amount       float64 `json:"amount"`
-	MerchantName string  `json:"merchantName"`
+	TransactionID    string      `json:"transactionId"`
+	CardID           string      `json:"cardId"`
+	MaskedCardNumber string      `json:"maskedCardNumber"`
+	CardName         string      `json:"cardName"`
+	Date             string      `json:"date"`
+	ShowTimestamp    bool        `json:"showTimestamp"`
+	Amount           float64     `json:"amount"`
+	Currency         string      `json:"currency"`
+	OriginalAmount   float64     `json:"originalAmount"`
+	OriginalCurrency string      `json:"originalCurrency"`
+	MerchantName     string      `json:"merchantName"`
+	PrettyName       string      `json:"prettyName"`
+	MerchantPlace    string      `json:"merchantPlace"`
+	IsOnline         bool        `json:"isOnline"`
+	PFMCategory      PFMCategory `json:"pfmCategory"`
+	StateType        string      `json:"stateType"`
+	Details          string      `json:"details"`
+	Type             string      `json:"type"`
+	IsBilled         bool        `json:"isBilled"`
+	Links            Links       `json:"links"`
+}
+
+type PFMCategory struct {
+	ID                  string `json:"id"`
+	Name                string `json:"name"`
+	LightColor          string `json:"lightColor"`
+	MediumColor         string `json:"mediumColor"`
+	Color               string `json:"color"`
+	ImageURL            string `json:"imageUrl"`
+	TransparentImageURL string `json:"transparentImageUrl"`
+}
+
+type Links struct {
+	Transactiondetails string `json:"transactiondetails"`
 }
 
 const URL_PRE = "https://api.one.viseca.ch/v1/card/"
@@ -55,10 +86,18 @@ func getTransactions(cardID, sessionCookie string) Transactions {
 }
 
 func printTransactions(transactions Transactions) {
-	fmt.Println("\"Date\",\"Payee\",\"Amount\"")
+	fmt.Println("\"TransactionID\",\"Date\",\"Merchant\",\"Amount\",\"PFMCategoryID\",\"PFMCategoryName\"")
 
 	for _, v := range transactions.Transactions {
-		fmt.Printf("\"%s\",\"%s\",\"%f\"\n", v.Date, v.MerchantName, v.Amount)
+		fmt.Printf("\"%s\",\"%s\",\"%s\",\"%f\",\"%s\",\"%s\"\n", v.TransactionID, v.Date, getPrettiestMerchantName(v), v.Amount, v.PFMCategory.ID, v.PFMCategory.Name)
+	}
+}
+
+func getPrettiestMerchantName(transaction Transaction) string {
+	if transaction.PrettyName != "" {
+		return transaction.PrettyName
+	} else {
+		return transaction.MerchantName
 	}
 }
 
